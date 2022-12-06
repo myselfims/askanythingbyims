bd16caf70564a9d9540daa5d9eab728b3062fde7
myselfims <104151760+myselfims@users.noreply.github.com>
2022-12-06 08:55:02 +0530

    Update settings.py

--- Polling_Website/settings.py
+++ Polling_Website/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static/')
