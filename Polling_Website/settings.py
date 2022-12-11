ca35b18eef4131c61c2b844bf2f8d06d52f4689b
myselfims <104151760+myselfims@users.noreply.github.com>
2022-12-11 18:42:40 +0530

    Update settings.py

--- Polling_Website/settings.py
+++ Polling_Website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qtf6ethwy56x-to@l25*j5a43icy*+_9lfgm=)!bbzuopv924t
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR / "static")
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
