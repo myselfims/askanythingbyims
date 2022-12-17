05f8b92a4df53ec483faa8c4d762e826115dd7ef
myselfims <shaikhimran7585@gmail.com>
2022-12-17 11:48:38 +0530

    clipboard copy update

--- static/js/js.js
+++ static/js/js.js
@@ -50,3 +50,18 @@ function SideBarToggle(){
     }
    
 };
+
+
+function unsecuredCopyToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error('Unable to copy to clipboard', err);
+    }
+    document.body.removeChild(textArea);
+  }
